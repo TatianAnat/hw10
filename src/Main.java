720921a67ae3bd897858875799ed893582508e2f
TatianAnat <TatianAnat@yandex.ru>
2025-04-22 20:49:03 +0400

    homework 10 task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        //task 1
+        System.out.println("task 1");
+        int [] weight = new int[3];
+        weight[0] = 1;
+        weight[1] = 2;
+        weight[2] = 3;
+        System.out.println(weight[0] );
+        System.out.println(weight[1]);
+        System.out.println(weight[2]);
+
+        float [] array = {1.57f, 7.654f, 9.986f};
+        System.out.println(array[0]);
+        System.out.println(array[1]);
+        System.out.println(array[2]);
+
+        byte [] array1 = {57, 54, 98, 110};
+        System.out.println(array1[0]);
+        System.out.println(array1[1]);
+        System.out.println(array1[2]);
+        System.out.println(array1[3]);
     }
 }
