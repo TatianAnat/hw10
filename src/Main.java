4c4d4acc080a8eb4154d1660bef38f7d83dc9266
TatianAnat <TatianAnat@yandex.ru>
2025-04-22 21:54:28 +0400

    homework 10 task 2

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,38 @@ public class Main {
     public static void main(String[] args) {
         //task 1
         System.out.println("task 1");
-        int [] weight = new int[3];
+        int[] weight = new int[3];
         weight[0] = 1;
         weight[1] = 2;
         weight[2] = 3;
-        System.out.println(weight[0] );
-        System.out.println(weight[1]);
-        System.out.println(weight[2]);
 
-        float [] array = {1.57f, 7.654f, 9.986f};
-        System.out.println(array[0]);
-        System.out.println(array[1]);
-        System.out.println(array[2]);
+        float[] array = {1.57f, 7.654f, 9.986f};
 
-        byte [] array1 = {57, 54, 98, 110};
-        System.out.println(array1[0]);
-        System.out.println(array1[1]);
-        System.out.println(array1[2]);
-        System.out.println(array1[3]);
+        byte[] array1 = {57, 54, 98, 110};
+
+
+        //task 2
+        for (int i = 0; i < weight.length; i++) {
+            System.out.print(weight[i]);
+            if (i < weight.length - 1) {
+                System.out.print(",");
+            }
+
+        }
+        System.out.println();
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
+            if (i < array.length - 1) {
+                System.out.print(",");
+            }
+
+        }
+        System.out.println();
+        for (int i = 0; i < array1.length; i++) {
+            System.out.print(array1[i]);
+            if (i < array1.length - 1) {
+                System.out.print(",");
+            }
+        }
     }
 }
